6372e4ce9175f56d2b3aaae095b68d2fafff9815
Griff <griffal@comcast.net>
2019-03-06 13:38:30 -0500

    Updates

---
+++ precache-manifest.0788a3f8615f735915dcbe0c18613770.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa105d66c22cf246fa41",
+    "url": "/clickytoo/static/js/runtime~main.fa105d66.js"
+  },
+  {
+    "revision": "d774e2cd3c76b32a1bf2",
+    "url": "/clickytoo/static/js/main.d774e2cd.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clickytoo/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "bd3b2e28d19cc596de24c4051152f7d8",
+    "url": "/clickytoo/index.html"
+  }
+];
